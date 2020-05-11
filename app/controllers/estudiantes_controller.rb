41c21279670aebe6815ac38b42f23a40c7954115
javier <ojalfarob@ujmd.edu.sv>
2020-05-11 17:39:06 -0600

    agregando vista new, edit y metodo delete

--- app/controllers/estudiantes_controller.rb
+++ app/controllers/estudiantes_controller.rb
@@ -3,4 +3,44 @@ class EstudiantesController < ApplicationController
         @estudens = Estudiante.all
     end
 
+    def new
+        @estudens = Estudiante.new
+    end
+
+    def create
+        @estudens = Estudiante.new(estudianes_params)
+        if @estudens.save
+            redirect_to estudiantes_index_path
+        else
+            render :new
+        end
+    end
+
+
+    def edit
+        @estudens = Estudiante.find(params[:id])
+    end
+
+    def update
+        @estudens = Estudiante.find(params[:id])
+        if @estudens.update(estudianes_params)
+            redirect_to estudiantes_index_path
+        else
+            render :edit
+        end
+    end
+
+    def delete
+        @estudens = Estudiante.find(params{:id})
+        if @estudens.destroy
+            redirect_to estudiantes_index_path
+        end
+
+    end
+
+    private
+    def estudianes_params
+        params.require(:estudiante).permit(:nombre,:edad)
+        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
 
   get 'estudiantes/index', to: 'estudiantes#index'
   get 'estudiantes/new', to: 'estudiantes#new'
+  post 'estuduantes/', to: 'estudiantes#create'
+  get 'estudiantes/:id', to: 'estudiantes#show', as: 'estudiante'
+  get 'estudiantes/:id/edit', to: 'estudiantes#edit', as: 'edit_estudiantes'
+  patch 'estudiantes/:id', to: 'estudiantes#update'
+  put 'estudiantes/:id', to: 'estudiantes#update'
+  delete 'estudiantes/:id', to: 'estudiantes#delete'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
