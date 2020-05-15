1571b04ed5be972a104d7e92365b56176d70eb9b
javier <ojalfarob@ujmd.edu.sv>
2020-05-15 11:07:56 -0600

    agregando vista index

--- app/controllers/mascotas_controller.rb
+++ app/controllers/mascotas_controller.rb
@@ -1,2 +1,23 @@
 class MascotasController < ApplicationController
+    def index
+        @mascotas = Mascotum.all
+    end
+
+    def new
+        @mascotas = Mascotum.new 
+    end
+
+    def create
+        @mascotas = Mascotum.new(pro_params)
+        if @mascotas.save
+        redirect_to mascotas_index_path
+        else
+           render :new
+        end
+    end
+
+
+    def pro_params
+        params.require(:mascotas).permit(:nombre,:descrip,:cantidad)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  get 'mascotas/index', to: 'mascotas#index'
+  get 'mascotas/new', to: 'mascotas#new'
+  post 'mascotas/create', to: 'mascotas#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
